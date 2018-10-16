876045b5fbec01c00083d91072635460f38c473d
Patrick <ctrlaltpat@gmail.com>
2018-10-16 16:25:25 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,13 @@
 Rails.application.routes.draw do
+  resources :songs
+
+  # get "/songs", to: 'songs#index'
+  # get "/songs/:id", to: 'songs#show'
+  # get "/songs/new", to: 'songs#new'
+  # post "/songs", to: 'songs#create'
+  # get "/songs/:id/edit", to: 'song#edit'
+  # patch "/songs/:id", to: 'song#update'
+  # delete "/songs/:id", to: 'song#destroy'
+
+
 end
